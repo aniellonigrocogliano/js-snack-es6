afc040aa55dd14ef509bbfb52d7b4a57804df7b4
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-04-15 16:43:08 +0200

    add: arry di oggetti di bicicletta

---
+++ Snack-1/js/script.js
@@ -0,0 +1,46 @@
+biciclette = [
+    {
+        nome: "Mountain Bike",
+        peso: 12
+    },
+    {
+        nome: "Bici da Corsa",
+        peso: 9
+    },
+    {
+        nome: "BMX",
+        peso: 10,
+    },
+    {
+        nome: "Bici da città",
+        peso: 15,
+    },
+    {
+        nome: "Bici pieghevole",
+        peso: 7,
+    },
+    {
+        nome: "Bici da montagna",
+        peso: 11,
+    },
+    {
+        nome: "Bici da viaggio",
+        peso: 13,
+    },
+    {
+        nome: "Bici da trekking",
+        peso: 14,
+    },
+    {
+        nome: "Bici da cicloturismo",
+        peso: 12,
+    },
+    {
+        nome: "Bici da pista",
+        peso: 8,
+    }
+];
+function name(bici) {
+    
+    
+}
